4035586e16614e09af371c811e26742abd75cb59
Huber, Marco <marco.huber@sivantos.com>
2021-03-27 23:48:04 +0100

    Add project files.

---
+++ RungeKutta/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RungeKutta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<double> linspace(double start, double end, int count) =>
+                Enumerable.Range(0, count).Select(x => start + x * (end - start) / count).ToList();
+
+            IList<double> arange(double start, double end, double step)
+            {
+                return Enumerable.Range(0, (int)Math.Round((end - start) / step)).Select(x => start + x * step).ToList();
+            }
+
+            DoubleVector model(DoubleVector state, double time, params object[] paramList)
+            {
+                var deltaState = new List<double>();
+                var y = state[0];
+                var v = state[1];
+
+                var aPar = (double)paramList[0];
+                var bPar = (double)paramList[1];
+                Func<double, double> func = (Func<double, double>)paramList[2];
+
+                var dydt = v;
+                var dvdt = -bPar * y - aPar * v + func(time);
+
+                return new DoubleVector(new List<double> { dydt, dvdt });
+            }
+
+            IList<DoubleVector> rungekutta4(Func<DoubleVector, double, object[], DoubleVector> func, DoubleVector y0, IList<double> time, params object[] paramList)
+            {
+                var n = time.Count;
+                var y = Enumerable.Repeat(0, n).Select(
+                    x => new DoubleVector(Enumerable.Repeat(0.0, y0.Count).ToList())).ToList();
+
+                y[0] = y0;
+
+                for(var i = 0; i < n -1; i++)
+                {
+                    var h = time[i + 1] - time[i];
+                    var k1 = func(y[i], time[i], paramList);
+                    var k2 = func(y[i] + ((k1 * h) / 2.0), time[i] + (h / 2.0), paramList);
+                    var k3 = func(y[i] + ((k2 * h) / 2.0), time[i] + (h / 2.0), paramList);
+                    var k4 = func(y[i] + (k3 * h), time[i] + h, paramList);
+                    y[i + 1] = y[i] + ((k1 + (k2 * 2)) + ((k3 * 2) + k4)) * (h / 6.0);
+                }
+
+                return y;
+            }
+
+            var T0 = 0.01; // in seconds
+            var Tend = 50; // in seconds
+            var t = arange(0, Tend, T0);
+
+            var D = 0.25;
+            var w0 = 1;
+            var a = 2 * D;
+            var b = Math.Pow(w0, 2);
+
+            Func<double, double> f = time  => time < 0 ? 0 : 1;
+
+            var y_rungekutta = rungekutta4(model, new DoubleVector(new List<double> { 0, 0 }), t, a, b, f);
+        }
+    }
+}
---
+++ RungeKutta/Vector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RungeKutta
+{
+    public class DoubleVector : List<double>
+    {
+        public DoubleVector(IEnumerable<double> list)
+        {
+            AddRange(list);
+        }
+
+        public static DoubleVector operator +(DoubleVector a, DoubleVector b)
+            => new DoubleVector(a.Select((value, index) => value + b[index]).ToList());
+
+        public static DoubleVector operator *(DoubleVector a, double b)
+            => new DoubleVector(a.Select((value, index) => value * b).ToList());
+
+        public static DoubleVector operator /(DoubleVector a, double b)
+            => new DoubleVector(a.Select((value, index) => value / b).ToList());
+    }
+}
