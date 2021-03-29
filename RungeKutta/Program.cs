dd11d192c3b60ce2f3659f5f0b7d087063069a3c
Huber, Marco <marco.huber@sivantos.com>
2021-03-29 20:46:23 +0200

    Comparison with analytical solution added

--- RungeKutta/Program.cs
+++ RungeKutta/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 namespace RungeKutta
 {
@@ -23,27 +24,55 @@ namespace RungeKutta
             Func<double, double> inputFunction = InputFunctions.Step;
 
             var y_rungekutta = RungeKutta4.Apply(
-                Models.HarmonicOsziallator, 
+                Models.HarmonicOsziallator,
                 new List<double> { 0, 0 }, t, a, b, inputFunction);
 
             var u = t.Select(time => inputFunction(time)).ToList();
-            var y = (IList<double>)y_rungekutta.Select(x => x[0]).ToList();
+            var y_rk = (IList<double>)y_rungekutta.Select(x => x[0]).ToList();
+
+            var y_analytical = t.Select(
+                time => 1 - Math.Pow(0.77880078307141, time) * Math.Cos(0.96824583655185 * time) -
+                0.258198888974715 * Math.Pow(0.77880078307141, time) * Math.Sin(0.96824583655185 * time)).ToList();
+
+            var delta_in_10_power_minus_10 = y_rk.Select((value_rk, index) => (value_rk - y_analytical[index]) * Math.Pow(10, 10)).ToList();
+            var y_rk_display = y_rk.Select(value => value * 10).ToList();
+            var u_display = u.Select(value => value * 10).ToList();
+
+            DisplayResults(t, u_display, y_rk_display, delta_in_10_power_minus_10);
+        }
+
+        private static void DisplayResults(IList<double> t, List<double> u, IList<double> y_rk, List<double> delta_in_10_power_minus_10)
+        {
+            CurveChartImageApi.Create(
+                            fileNameWithoutExtention: "RungeKutta",
+                            headerCaption: "HarmonicOsziallor Input and Output in 10^(-1)",
+                            xGrid1: t,
+                            xGrid2: t,
+                            curveList1: new List<IList<double>> { u },
+                            curveList2: new List<IList<double>> { y_rk },
+                            outputDir: "RungeKutta",
+                            linearFreqAxis: true
+                            );
 
             CurveChartImageApi.Create(
-                fileNameWithoutExtention:"RungeKutta",
-                headerCaption: "HarmonicOsziallor",
-                xGrid1: t,
+                fileNameWithoutExtention: "RungeKutta_Delta",
+                headerCaption: "HarmonicOsziallor delta to analytical solution in 10^(-10)",
+                xGrid1: null,
                 xGrid2: t,
-                curveList1: new List<IList<double>> { u },
-                curveList2: new List<IList<double>> { y },
-                outputDir:"RungeKutta",
-                linearFreqAxis:true
+                curveList1: null,
+                curveList2: new List<IList<double>> { delta_in_10_power_minus_10 },
+                outputDir: "RungeKutta",
+                linearFreqAxis: true
                 );
 
             CloseInternetExplorer();
             StartInternetExplorerwithPngFile("RungeKutta", "RungeKutta");
-        }
 
+            Console.Write("Press any key");
+            Console.ReadKey(true);
+
+            StartInternetExplorerwithPngFile("RungeKutta", "RungeKutta_Delta");
+        }
 
         private static void StartInternetExplorerwithPngFile(string outputFolderName, string fileNameWithoutExtention)
         {
--- RungeKutta/RungeKutta4.cs
+++ RungeKutta/RungeKutta4.cs
@@ -6,9 +6,9 @@ namespace RungeKutta
 {
     public class RungeKutta4
     {
-        public static IList<IList<double>> Apply(Func<IList<double>, double, object[], IList<double>> func, IList<double> y0, IList<double> time, params object[] paramList)
+        public static IList<IList<double>> Apply(Func<IList<double>, double, object[], IList<double>> func, IList<double> y0, IList<double> t, params object[] paramList)
         {
-            var n = time.Count;
+            var n = t.Count;
             var y = Enumerable.Repeat(0, n).Select(
                 x => new DoubleVector(Enumerable.Repeat(0.0, y0.Count).ToList())).ToList();
 
@@ -16,11 +16,11 @@ namespace RungeKutta
 
             for (var i = 0; i < n - 1; i++)
             {
-                var h = time[i + 1] - time[i];
-                var k1 = new DoubleVector(func(y[i], time[i], paramList));
-                var k2 = new DoubleVector(func(y[i] + k1 * h / 2.0, time[i] + h / 2.0, paramList));
-                var k3 = new DoubleVector(func(y[i] + k2 * h / 2.0, time[i] + h / 2.0, paramList));
-                var k4 = new DoubleVector(func(y[i] + k3 * h, time[i] + h, paramList));
+                var h = t[i + 1] - t[i];
+                var k1 = new DoubleVector(func(y[i], t[i], paramList));
+                var k2 = new DoubleVector(func(y[i] + k1 * h / 2.0, t[i] + h / 2.0, paramList));
+                var k3 = new DoubleVector(func(y[i] + k2 * h / 2.0, t[i] + h / 2.0, paramList));
+                var k4 = new DoubleVector(func(y[i] + k3 * h, t[i] + h, paramList));
                 y[i + 1] = y[i] + (k1 + k2 * 2 + k3 * 2 + k4) * (h / 6.0);
             }
 
