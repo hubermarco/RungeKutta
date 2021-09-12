f5c36da373b13f65a6980b012729af1df4c8c19c
Huber, Marco <marco.huber@sivantos.com>
2021-09-12 11:16:06 +0200

    Automatic Scaling intrduced

---
+++ CurveChartImageCreator/Api/Scaling.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CurveChartImageCreater
+{
+    public class Scaling
+    {
+        public static void Apply(
+           IList<double> curve,
+           int minExponent,
+           int maxExponent,
+           ref IList<double> curveOutput,
+           ref int scalingExponent)
+        {
+            int rangeExponent = CalculateRangeExponent(curve);
+            int skalingExponentResulting = CalculateResultingSkalingExponent(minExponent, maxExponent, rangeExponent);
+
+            curveOutput = curve?.Select(x => x * Math.Pow(10, skalingExponentResulting)).ToList();
+            scalingExponent = -skalingExponentResulting;
+        }
+
+        public static void ApplyWithOutput(
+           IList<double> curve,
+           int minExponent,
+           int maxExponent,
+           out IList<double> curveOutput,
+           out int scalingExponent)
+        {
+            scalingExponent = 0;
+            curveOutput = new List<double>();
+
+            Apply(
+                curve,
+                minExponent,
+                maxExponent,
+                ref curveOutput,
+                ref scalingExponent);
+        }
+
+        public static void Apply(
+            IList<double> curve1, 
+            IList<double> curve2,
+            int minExponent,
+            int maxExponent,
+            ref IList<double> curveOutput1,
+            ref IList<double> curveOutput2,
+            ref int scalingExponent)
+        {
+            var rangeExponent1 = ((curve1 != null) && (curve1.Count > 0)) ? CalculateRangeExponent(curve1) : int.MinValue;
+            var rangeExponent2 = ((curve2 != null) && (curve2.Count > 0)) ? CalculateRangeExponent(curve2) : int.MinValue;
+
+            var resultingRangeExponent = (rangeExponent1 > rangeExponent2) ? rangeExponent1 : rangeExponent2;
+            var correctedResultingRangeExponent = (resultingRangeExponent == int.MinValue) ? 0 : resultingRangeExponent;
+            var skalingExponentResulting = CalculateResultingSkalingExponent(minExponent, maxExponent, correctedResultingRangeExponent);
+
+            curveOutput1 = curve1?.Select(x => x * Math.Pow(10, skalingExponentResulting)).ToList();
+            curveOutput2 = curve2?.Select(x => x * Math.Pow(10, skalingExponentResulting)).ToList();
+            scalingExponent = -skalingExponentResulting;
+        }
+
+        public static void ApplyWithOutput(
+            IList<double> curve1,
+            IList<double> curve2,
+            int minExponent,
+            int maxExponent,
+            out IList<double> curveOutput1,
+            out IList<double> curveOutput2,
+            out int scalingExponent)
+        {
+            scalingExponent = 0;
+            curveOutput1 = new List<double>();
+            curveOutput2 = new List<double>();
+
+            Apply(
+                curve1,
+                curve2,
+                minExponent,
+                maxExponent,
+                ref curveOutput1,
+                ref curveOutput2,
+                ref scalingExponent);
+        }
+
+        private static int CalculateRangeExponent(IList<double> curve)
+        {
+            var range = ( (curve == null) || (curve.Count == 0)) ? 0 : curve.Max() - curve.Min();
+            var usedRange = (range != 0) ? range : 1;
+            var exponent = Math.Log10(Math.Abs(usedRange));
+            var roundedExponent = exponent < 0 ? (int)Math.Floor(exponent) : (int)Math.Floor(exponent);
+            return roundedExponent;
+        }
+
+        private static int CalculateResultingSkalingExponent(int minExponent, int maxExponent, int rangeExponent)
+        {
+            return (rangeExponent > 0) ?
+                ((minExponent <= rangeExponent) && (rangeExponent <= maxExponent)) ? 0 : maxExponent - rangeExponent:
+                ((minExponent <= rangeExponent) && (rangeExponent <= maxExponent)) ? 0 : minExponent - rangeExponent;
+        }
+    }
+}
--- RungeKutta/Program.cs
+++ RungeKutta/Program.cs
@@ -1,4 +1,5 @@
-﻿using CurveChartImageCreator;
+﻿using CurveChartImageCreater;
+using CurveChartImageCreator;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -42,13 +43,25 @@ namespace RungeKutta
 
         private static void DisplayResults(IList<double> t, List<double> u, IList<double> y_rk, List<double> delta)
         {
-            var y_rk_display = y_rk.Select(value => value * 10).ToList();
-            var u_display = u.Select(value => value * 10).ToList();
-            var delta_in_10_power_minus_10 = delta.Select(value => value * Math.Pow(10,10)).ToList();
+            Scaling.ApplyWithOutput(
+                curve1: y_rk,
+                curve2: u,
+                maxExponent: 1,
+                minExponent: 1,
+                curveOutput1: out var y_rk_display,
+                curveOutput2: out var u_display,
+                scalingExponent: out var scalingExponent);
+
+            Scaling.ApplyWithOutput(
+                curve: delta,
+                minExponent: 1,
+                maxExponent: 1,
+                curveOutput: out var delta_in_10_power_minus_10,
+                scalingExponent: out var scalingExponentDelta);
 
             CurveChartImageApi.Create(
                             fileNameWithoutExtention: "RungeKutta",
-                            headerCaption: "HarmonicOsziallor Input and Output in 10^(-1)",
+                            headerCaption: $"HarmonicOsziallor Input and Output in 10^({scalingExponent})",
                             xGrid1: t,
                             xGrid2: t,
                             curveList1: new List<IList<double>> { u_display },
@@ -59,7 +72,7 @@ namespace RungeKutta
 
             CurveChartImageApi.Create(
                 fileNameWithoutExtention: "RungeKutta_Delta",
-                headerCaption: "HarmonicOsziallor delta to analytical solution in 10^(-10)",
+                headerCaption: $"HarmonicOsziallor delta to analytical solution in 10^({scalingExponentDelta})",
                 xGrid1: null,
                 xGrid2: t,
                 curveList1: null,
