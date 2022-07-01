d0df65f28eb66c5f4b5b92ef5e75bc6677dd080e
Huber, Marco <marco.huber@sivantos.com>
2022-07-01 11:19:01 +0200

    == added to DoubleVector

--- RungeKutta/DoubleVector.cs
+++ RungeKutta/DoubleVector.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace RungeKutta
 {
     internal class DoubleVector : List<double>
     {
+        private static double _tolerance = Math.Pow(10, -6);
+
         public DoubleVector(IEnumerable<double> list)
         {
             AddRange(list);
@@ -13,6 +16,23 @@ namespace RungeKutta
         public DoubleVector(params double[] paramList)
         {
             AddRange(paramList);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is DoubleVector vector &&
+                   Capacity == vector.Capacity &&
+                   Count == vector.Count;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
         }
 
         public static DoubleVector operator +(DoubleVector a, DoubleVector b)
@@ -26,5 +46,11 @@ namespace RungeKutta
 
         public static DoubleVector operator /(DoubleVector a, double b)
             => new DoubleVector(a.Select(value => value / b).ToList());
+
+        public static bool operator ==(DoubleVector a, DoubleVector b)
+            => (a.Count == b.Count) && a.Select((aValue, index) => Math.Abs(aValue - b[index])).All(x => x < _tolerance);
+
+        public static bool operator !=(DoubleVector a, DoubleVector b)
+            => (a.Count != b.Count) || a.Select((aValue, index) => Math.Abs(aValue - b[index])).Any(x => x > _tolerance);
     }
 }
